920e57367071a7a5653b42f3e6f4ac20baff684d
Nathan Rossow <nathanrossow@gmail.com>
2026-08-24 16:43:57 -0500

    Add new categories for Top Picks and apparel

---
+++ categories.py
@@ -0,0 +1,23 @@
+T-Shirts
+Polos/Knits
+Sweatshirts/Fleece
+Caps
+Activewear
+Outerwear
+Woven Shirts
+Bags
+Workwear
+Accessories
+Personal Protection
+Ladies
+Youth
+
+Top Picks
+
+T - Shirt
+Polos
+Sweatshirts
+Caps
+Outerwear
+Shirts
+

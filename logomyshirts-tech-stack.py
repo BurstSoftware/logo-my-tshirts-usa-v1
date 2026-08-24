a00a93e1ff4d0bdb3c07c6c03a225df5ebf47df4
Nathan Rossow <nathanrossow@gmail.com>
2026-08-24 16:56:05 -0500

    Update logomyshirts-tech-stack.py

--- logomyshirts-tech-stack.py
+++ logomyshirts-tech-stack.py
@@ -1,40 +1,19 @@
-Website Technology & Feature Report 
-URL: https://logomyshirts.comDetected Technologies & Features (non-empty columns only)Category
-Value
-Miscellaneous
-RSS ; Open Graph ; HTTP/3
-Security
-reCAPTCHA
-Font scripts
-Font Awesome ; Twitter Emoji (Twemoji)
-CDN
-jsDelivr ; cdnjs ; Cloudflare
-JavaScript libraries
-Select2 ; jQuery UI ; jQuery Migrate ; jQuery ; DataTables ; core-js
-Performance
-Priority Hints
-SEO
-Yoast SEO
-PaaS
-Amazon Web Services
-WordPress plugins
-Gravity Forms ; Essential Addons for Elementor ; Elementor Header & Footer Builder ; WooCommerce ; Elementor ; Yoast SEO
-Form builders
-Gravity Forms
-UI frameworks
-ZURB Foundation
-WordPress themes
-Astra
-Programming languages
-PHP
-Databases
-MySQL
-CMS
-WordPress
-Ecommerce
-WooCommerce
-Blogs
-WordPress
-Page builder
-Elementor
-
+Category,Value
+Miscellaneous,"RSS ; Open Graph ; HTTP/3"
+Security,reCAPTCHA
+Font scripts,"Font Awesome ; Twitter Emoji (Twemoji)"
+CDN,"jsDelivr ; cdnjs ; Cloudflare"
+JavaScript libraries,"Select2 ; jQuery UI ; jQuery Migrate ; jQuery ; DataTables ; core-js"
+Performance,Priority Hints
+SEO,Yoast SEO
+PaaS,Amazon Web Services
+WordPress plugins,"Gravity Forms ; Essential Addons for Elementor ; Elementor Header & Footer Builder ; WooCommerce ; Elementor ; Yoast SEO"
+Form builders,Gravity Forms
+UI frameworks,ZURB Foundation
+WordPress themes,Astra
+Programming languages,PHP
+Databases,MySQL
+CMS,WordPress
+Ecommerce,WooCommerce
+Blogs,WordPress
+Page builder,Elementor

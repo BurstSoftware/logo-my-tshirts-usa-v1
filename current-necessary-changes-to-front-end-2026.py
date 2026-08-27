501d2d92cad3fd08a40923db100edcd82d59487b
Nathan Rossow <nathanrossow@gmail.com>
2026-08-27 12:23:09 -0500

    Create current-necessary-changes-to-front-end-2026.py

---
+++ current-necessary-changes-to-front-end-2026.py
@@ -0,0 +1,8 @@
+# Change or modify the look of the list of colors under the category menu listing to fit the design and increase the user experience
+
+# Make the search bar at the top black outlined
+
+# For the blue background and white text
+
+# Make the background black
+# And the font larger and a thicker font, bolder font
